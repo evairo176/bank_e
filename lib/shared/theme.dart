c7330b8dcafae9e999b5c92e234b541161c9cfaf
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-03-04 08:25:19 +0700

    feat: update

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// list color
 Color whiteColor = const Color(0xffFFFFFF);
 Color blackColor = const Color(0xff14193F);
 Color greyColor = const Color(0xffA4A8AE);
@@ -11,12 +12,14 @@ Color purpleColor = const Color(0xff5142E6);
 Color greenColor = const Color(0xff22B07D);
 Color numberBackgroundColor = const Color(0xff1A1D2E);
 
+// list text with color
 TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
 TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
 TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
 TextStyle blueTextStyle = GoogleFonts.poppins(color: blueColor);
 TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
 
+// list text syle
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
