f27c75249e39fa33e85bdae9759215cfe21b1f2c
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-03-08 09:12:33 +0700

    feat: update

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+// buttom navigation
   @override
   Widget build(BuildContext context) {
     return Scaffold(
